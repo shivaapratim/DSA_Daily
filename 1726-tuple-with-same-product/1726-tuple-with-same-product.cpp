be311412a50578ec3617af437f40a222e9cd7c25
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2025-02-06 20:47:50 +0530

    Time: 330 ms (41.69%), Space: 84.9 MB (81.5%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        // using concept of if we know 3 we can know the other 1
-        // O(n^3)
-        sort(nums.begin(),nums.end());
-        int tup=0;
-        int n=nums.size();
-        for(int i=0; i<n;i++){
-            for(int j=n-1; j>i; j--){
-                // set to store the value of product 
-                unordered_set<int>st;
-                int product= nums[i]*nums[j];
-                for(int k=i+1; k<j; k++){
-
-                    if(product%nums[k]==0){
-                        int want=product/nums[k];
-                        if(st.count(want)){
-                            tup++;
-                        }
-                        st.insert(nums[k]);
-                    }
-                    
-                }
+        int n= nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                mp[nums[i]*nums[j]]++;
             }
         }
-        return tup*8;
+        int tup=0;
+
+        for(auto it: mp){
+            if(it.second>=2){
+                int t= it.second*(it.second-1)/2;
+                tup+=t*8;
+            }
+        }
+        return tup;
     }
 };
