7666c03440be62f1083528c6f6e2678053983da4
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2025-02-07 17:38:56 +0530

    Time: 126 ms (44.05%), Space: 157.6 MB (56.59%) - LeetHub

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -6,7 +6,7 @@ public:
         vector<int>res;
         int dis=0;
 
-        for(auto it: queries){
+        for(auto&it: queries){
             int a= it[0];
             int b= it[1];
 
@@ -14,7 +14,14 @@ public:
             if (mp.find(a) == mp.end()) {
                 mp[a] = 0;
             }
+            // Two cases either colored , or not colored
 
+            //If colored then check for the correstponding color count and then if it is
+            //Equal to 0 toh distinct--, else no farak Also update the new one with checking
+            //THe same >0 and 0 for the new being colored(b)  if yes then >0 or 0 times , if 0 then increment distinct else no
+ 
+            //If non colored then surely color, and also check if that color is in st map
+            // if yes then >0 or 0 times , if 0 then increment distinct else no
             if(mp[a]==0){
                 if(st[b]==0){
                     st[b]++;
