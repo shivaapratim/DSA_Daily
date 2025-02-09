290a4064cc31cfa94b0603d112ee44da9d31926a
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2025-02-09 13:48:16 +0530

    Time: 64 ms (75.68%), Space: 87.5 MB (87.39%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
+        
+        //It means-> nums[i]-i!=nums[j]-j
+
+        //But if u count bad, u will have to use O(n^2) anyhow!
+
+        //So first count the good and then from total...-good=bad
+
         long long countGood=0;
 
         unordered_map<int,int>mp;
