4f71f568037a3d39c72db2a9717a85494f4073ab
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2025-02-10 18:07:41 +0530

    Time: 0 ms (100%), Space: 8.4 MB (87.14%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+      int k = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] > '9') {
+                s[k++] = s[i];
+            }
+            else {
+                --k;
+            }
+        }
+
+        s.resize(k);
+        return move(s);
+    }
+};
