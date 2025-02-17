ecf4ad1f0b4eea1112f2b341c8c81b51adb2ff55
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2025-02-17 15:11:35 +0530

    Time: 5 ms (55.61%), Space: 7.8 MB (97.61%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    set<string>st;
+    // set<string>st;
 
-    void solve(string&curr, vector<bool>&used, string&tiles){
+    // void solve(string&curr, vector<bool>&used, string&tiles){
         
-        st.insert(curr);
+    //     st.insert(curr);
 
-        for(int i=0; i<tiles.size(); i++){
-            if(used[i])continue;
+    //     for(int i=0; i<tiles.size(); i++){
+    //         if(used[i])continue;
 
-            curr.push_back(tiles[i]);
-            used[i]=true;
-            solve(curr,used,tiles);
+    //         curr.push_back(tiles[i]);
+    //         used[i]=true;
+    //         solve(curr,used,tiles);
 
-            //Undo
-            curr.pop_back();
-            used[i]=false;
+    //         //Undo
+    //         curr.pop_back();
+    //         used[i]=false;
+    //     }
+    //     return;
+    // }
+
+
+    int n=0;
+    void solve(vector<int>&v, int&cnt){
+        cnt++;
+
+        for(int i=0; i<26; i++){
+            if(v[i]==0)continue;    
+            v[i]--;
+            solve(v,cnt);
+            v[i]++;
         }
         return;
     }
@@ -23,12 +37,24 @@ public:
 
     int numTilePossibilities(string tiles) {
        
-        int n=tiles.size();
-        vector<bool>used(n,false);
+        n=tiles.size();
+        
+        // vector<bool>used(n,false);
 
-        string curr="";
-        solve(curr,used,tiles);
-        return st.size()-1; //Exculed the empty sequence
+        // string curr="";
+        // solve(curr,used,tiles);
+        // return st.size()-1; //Exculed the empty sequence
+
+
+        //2nd approach
+
+        vector<int>v(26);
+        for(int i=0; i<tiles.size();i++){
+            v[tiles[i]-'A']++;
+        }
+        int cnt=0;
+        solve(v,cnt);
+        return cnt-1;
         
     }
 };
