866e146c77aa8e3c4b5b854a7e718806d6d3ab24
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2025-03-25 11:38:25 +0530

    Time: 3 ms (31.03%), Space: 21.7 MB (45.85%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
-    int call(int it, vector<vector<int>>& intervals, int e) {
-        for (auto t : intervals) {
-            if (t[1] > it) {
-                return max(t[1], e);
-            }
-        }
-        return e;
-    }
-
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> arr;
-        
-        int s = newInterval[0];
-        int e = newInterval[1];
-        bool inserted = false;
-
-        for (auto it : intervals) {
-            if (it[1] < s) {
-                arr.push_back(it);
-            } else if (it[0] > e) {
-                if (!inserted) {
-                    arr.push_back({s, e});
-                    inserted = true;
-                }
-                arr.push_back(it);
-            } else {
-                s = min(s, it[0]);
-                e = max(e, it[1]);
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0; i<intervals.size(); i++){
+            if(ans.empty() || ans.back()[1]<intervals[i][0]){
+                ans.push_back({intervals[i][0],intervals[i][1]});
+            }
+            else if(ans.back()[1]>=intervals[i][0]){
+                ans.back()[1]= max(ans.back()[1],intervals[i][1]);
             }
         }
 
-        if (!inserted) {
-            arr.push_back({s, e});
-        }
-
-        return arr;
+        return ans;
     }
 };
