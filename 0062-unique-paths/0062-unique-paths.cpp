e6410e7d555973250dd7c730b6a3d0e59dcb0c20
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2025-09-27 14:17:12 +0530

    Time: 3 ms (8.05%), Space: 10.2 MB (8.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
 
-    vector<vector<int>>dir= {{0,1},{1,0}};
-    int dp[101][101];
+    int dp[201][201];
+    vector<vector<int>>dir={{0,1},{1,0}};
 
     int solve(int i, int j, int m, int n){
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
+        if(i==m-1 && j==n-1)return 1;
 
-        if(i>=m || i<0 || j>=n || j<0){
+        if(i>=m || j>=n || j<0 || i<0){
             return 0;
         }
 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        int ans=0;
+
+        for(auto it: dir){
+            int i_= it[0]+i;
+            int j_= it[1]+j;
+
+            ans+=solve(i_,j_,m,n);
         }
 
-        int ways=0;
-
-        for(auto it : dir){
-            int i_=it[0];
-            int j_=it[1];
-            ways+=solve(i+i_,j+j_,m,n);
-        }
-        return dp[i][j]=ways;
+        return dp[i][j] = ans;
     }
 
-    int uniquePaths(int m, int n) {
-        memset(dp,-1,sizeof(dp));
+    int uniquePaths(int m, int n) { 
+        memset(dp,-1,sizeof(dp)); 
+
         return solve(0,0,m,n);
     }
 };
