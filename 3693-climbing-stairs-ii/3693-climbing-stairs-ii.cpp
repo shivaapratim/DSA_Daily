c53527f646c49c08042b90efab09797ca3069f71
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2025-09-29 00:11:41 +0530

    Time: 48 ms (11.41%), Space: 185.6 MB (12.5%) - LeetHub

---
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> memo;
+
+    int solve(int i, vector<int>& costs) {
+        if (i == 0) return 0;
+        if (memo[i] != -1) return memo[i];
+
+        int res = INT_MAX;
+
+        // jump of 1
+        if (i - 1 >= 0)
+            res = min(res, solve(i - 1, costs) + costs[i - 1] + 1);
+
+        // jump of 2
+        if (i - 2 >= 0)
+            res = min(res, solve(i - 2, costs) + costs[i - 1] + 4);
+
+        // jump of 3
+        if (i - 3 >= 0)
+            res = min(res, solve(i - 3, costs) + costs[i - 1] + 9);
+
+        return memo[i] = res;
+    }
+
+    int climbStairs(int n, vector<int>& costs) {
+        memo.assign(n + 1, -1);
+        return solve(n, costs);
+    }
+};
