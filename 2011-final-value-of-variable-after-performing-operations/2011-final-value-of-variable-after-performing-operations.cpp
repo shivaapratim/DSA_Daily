3edf98468a2b91f41a8ae6a66ef3bf6f2c23991f
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2025-10-20 08:22:17 +0530

    Time: 0 ms (100%), Space: 17.5 MB (74.44%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+
+        for(auto it: operations){
+            if(it[1]=='+'){
+                x++;
+            }
+            else{
+                x--;
+            }
+        }
+
+        return x;
+    }
+};
