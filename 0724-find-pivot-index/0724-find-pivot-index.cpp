cba0531d27d149de1392bf0c2444cc11547cf13d
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2026-02-11 10:13:36 +0530

    Time: 0 ms (100%), Space: 37 MB (10.96%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>left(n,0);
+        vector<int>right(n,0);
+
+        left[0]=nums[0];
+        right[n-1]=nums[n-1];
+
+        // ------> left[i-1] +nums[i]
+        // right[i+1] +nums[i] <------- 
+
+        for(int i=1; i<n; i++){
+            left[i]=nums[i]+left[i-1];
+            
+        }
+    
+        for(int i=n-2; i>=0; i--){
+            right[i]=nums[i]+right[i+1];
+            
+        }
+
+        int ans=-1;
+
+        for(int i=0; i<n; i++){
+            if(right[i]==left[i]){
+                ans=i;
+                break;
+            }
+             
+        }
+         
+        return ans==-1 ? -1 : ans;
+    }
+};
+
