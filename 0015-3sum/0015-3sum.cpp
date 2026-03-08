4b71f3ff4a34c22ea5ddbac2271a4d6f963d9270
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2026-03-08 21:24:50 +0530

    Time: 51 ms (45.8%), Space: 28.9 MB (90.98%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -15,10 +15,11 @@ public:
                 else if(sum>0)k--;
                 else if(sum==0){
                 ans.push_back({nums[i],nums[j],nums[k]});
-                j++;
-                k--;
-                while(j<k && nums[j]==nums[j-1])j++;
-                while(j<k && nums[k]==nums[k+1])k--;
+                
+                int s=j;
+                while(j<k && nums[j]==nums[s])j++;
+                s=k;
+                while(j<k && nums[k]==nums[s])k--;
                 }
             }
         }
