b8cce12ea051404620d95dda718496eb9037ed3f
Shivang Shukla <119624728+shivaapratim@users.noreply.github.com>
2026-04-03 11:41:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,7 +1,18 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        return word.equals(word.toUpperCase()) || 
-               word.equals(word.toLowerCase()) || 
-               word.equals(word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase());
+        String upperWord=word.toUpperCase();
+        String lower = word.toLowerCase();
+        String onecap= word.substring(0,1).toUpperCase()+word.substring(1).toLowerCase();
+        
+        if(word==upperWord){
+            return true;
+        }
+        else if(word==lower){
+            return true;
+        }
+        else if(word==onecap){
+            return true;
+        }
+       return false;
     }
 }
